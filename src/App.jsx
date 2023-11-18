a83850febf6e420a5d944944c230109f2cb06cec
João Pedro Morbeck <jpmorbeck45@gmail.com>
2023-11-18 20:28:05 -0300

    adicionando função de adicionar jogos favoritos

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Game from "./components/Game/Game";
 
 function App() {
   const [gameList, setGameList] = useState([]);
+  const [myGames, setMyGames] = useState([]);
 
   const fetchData = async () => {
     await fetch("./games.json")
@@ -10,16 +11,27 @@ function App() {
         return res.json();
       })
       .then((data) => {
-        console.log(data);
         setGameList(data.games);
       });
+  };
+
+  const handleAddGame = (game) => {
+    let myGamesList = [...myGames];
+    myGamesList.push({
+      id: game.id,
+      name: game.name,
+      genre: game.genre,
+    });
+    setMyGames(myGamesList);
   };
 
   const listItems = gameList.map((game) => {
     return (
       <li key={game.id}>
-        <Game name={game.name} image={game.image} genre={game.genre}/>
-        <button type="button">+</button>
+        <Game name={game.name} image={game.image} genre={game.genre} />
+        <button type="button" onClick={() => handleAddGame(game)}>
+          +
+        </button>
       </li>
     );
   });
@@ -32,7 +44,15 @@ function App() {
     <>
       <h1>RecomendaGames</h1>
       <p>Seus jogos favoritos:</p>
-
+      <p>
+        {myGames.map((game, index) => (
+          <div key={index}>
+            <p>ID: {game.id}</p>
+            <p>Name: {game.name}</p>
+            <p>Genre: {game.genre}</p>
+          </div>
+        ))}
+      </p>
       <ul>{listItems}</ul>
     </>
   );
